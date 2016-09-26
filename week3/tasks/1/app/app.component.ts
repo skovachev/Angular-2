e5c21b0c01a10fdb6a68a175fcb829a9a3f30bc8
Stefan Kovachev <stefan.kovachev@gmail.com>
2016-09-26 19:53:30 +0300

    Added blank project for week3 task 1

---
+++ week3/tasks/1/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewChild } from '@angular/core'
+
+@Component({
+	moduleId: module.id,
+	selector: 'app',
+	templateUrl: 'app.component.html',
+	styleUrls: ['app.component.css']
+})
+export class App {
+
+	
+}
